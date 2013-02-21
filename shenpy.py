2694a8fae8bfd8f5dc1bd0684b9c03b1f9179061
Ramil Farkhshatov <ramil@gmx.co.uk>
2013-02-21 15:26:49 +0400

    Updated error handling. Moved standard functions into separate file.

--- shenpy.py
+++ shenpy.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 
 type_function = object()
 type_symbol = object()
@@ -15,11 +16,13 @@ reg = [None]
 save_pc = None
 nargs = 0
 
+show_step = False
+
 def stack_size(size):
     global stack, sp
     s = len(stack)
-    if s + sp <= size:
-        stack.extend([None] * size)
+    if sp + size + 1 > s:
+        stack.extend([None] * (sp + size - s + 1))
 
 def reg_size(size):
     global reg
@@ -27,59 +30,49 @@ def reg_size(size):
         reg.extend([None] * (size - len(reg) + 1))
 
 def run():
-    global save_pc
+    global save_pc, error_obj, fns, show_step, nargs, sp, stack, reg
     while save_pc:
         try:
             pc = save_pc
             save_pc = None
             while pc:
                 #print(pc)
-                #dbg_show_step(pc)
+                if show_step:
+                    dbg_show_step(pc)
                 pc = pc()
         except Exception as error:
-            print("error: {0}".format(error))
-            if call_error_handler(error) == None:
+            #print("**ERROR: {0}".format(error))
+            #traceback.print_exc()
+            if len(error_handlers) == 0:
                 raise
+            #dbg_show_step("ERROR HANDLER")
+            error_obj = error
+            nargs = 0
+            save_pc = fns["klvm-call-error-handler"]()
 
 def err(str):
     raise Exception(str)
 
-def set_error_handler(e):
+def push_error_handler(e):
     global error_handlers, sp, reg, nargs
-    error_handlers.append([sp, nargs, e])
+    #dbg_show_step("SET ERROR HANDLER")
+    error_handlers.append([sp, nargs, reg[0], e])
 
-def call_error_handler(e):
+def pop_error_handler():
+    global error_handlers
+    error_handlers.pop()
+
+def default_error_handler():
+    global error_obj
+    raise error_obj
+
+def error_unwind_get_handler():
     global error_handlers, sp, reg, error_obj
-    while len(error_handlers) > 0:
-        x = error_handlers.pop()
-        if x[0] <= sp:
-            error_obj = e
-            sp = x[0]
-            nargs = x[1]
-            save_pc = x[2]
-            return True
-    return None
-
-def cons__question_():
-    #dbg_show_step('cons?')
-    ret = (type(reg[1]) == list) and (len(reg[1]) > 0)
-    ret = ret and (reg[1][0] == type_cons)
-    reg[1] = ret
-    return reg[0]
-
-fns['cons?'] = cons__question_
-
-def tail():
-    reg[1] = reg[1][2]
-    return reg[0]
-
-fns['tail'] = tail
-
-def __plus_():
-    reg[1] = reg[1] + reg[2]
-    return reg[0]
-
-fns['+'] = __plus_
+    x = error_handlers.pop()
+    sp = x[0]
+    nargs = x[1]
+    reg[0] = x[2]
+    return x[3]
 
 dbg_step = False
 
--- t2.py
+++ t2.py
@@ -1,23 +1,39 @@
 import imp
 import shenpy
 import runtime
+import std
 import list
 
 imp.reload(shenpy)
 imp.reload(runtime)
+imp.reload(std)
 imp.reload(list)
 
-def t(func, *args):
+shenpy.show_step = False
+
+def t(expr, result = None):
     print("")
+    func = expr[0]
+    args = expr[1:]
     str_args = " ".join(tuple(map(str, args)))
     print("CALLING: ({0} {1})".format(func.__name__, str_args))
     ret = shenpy.call(func, *args)
     print("=> {0}".format(ret))
+    if result == None:
+        return
+    elif result == ret:
+        print("OK")
+    else:
+        print("Error: expected {0}".format(result))
 
-t(shenpy.fns["list-len"], shenpy.dbg_list([1, 2, 3, 4, 5, 6]))
-t(shenpy.fns["list-len*"], shenpy.dbg_list([1, 2, 3, 4, 5, 6]))
-t(shenpy.fns["use-adder"], 5, 7)
-t(shenpy.fns["use-freeze"], 11)
-t(shenpy.fns["wo-trap-error"], 19)
-t(shenpy.fns["use-trap-error-1"], 19)
-t(shenpy.fns["use-trap-error-2"], 91)
+t([shenpy.fns["list-len"], shenpy.dbg_list([1, 2, 3, 4, 5, 6])], 6)
+t([shenpy.fns["list-len*"], shenpy.dbg_list([1, 2, 3, 4, 5, 6])], 6)
+t([shenpy.fns["use-adder"], 5, 7], 12)
+t([shenpy.fns["use-adder"], -5, 7], 2)
+t([shenpy.fns["use-freeze"], 11], 21)
+#shenpy.show_step = True
+t([shenpy.fns["wo-trap-error"], 19], 19 + 19)
+t([shenpy.fns["use-trap-error-1"], 19], 19 * 2)
+t([shenpy.fns["use-trap-error-2"], 91], -1)
+t([shenpy.fns["use-trap-error-3"], 91], -2)
+t([shenpy.fns["use-trap-error-4"], 91], 0)
